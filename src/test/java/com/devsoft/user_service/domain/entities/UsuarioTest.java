dde00bca16a37b69218ea0cd65d73ab93b42dd50
ManuelArango1229 <juan12292004@outlook.com>
2025-03-22 19:28:08 -0500

    test: implementacion de test para el usuario (dominio)

---
+++ src/test/java/com/devsoft/user_service/domain/entities/UsuarioTest.java
@@ -0,0 +1,27 @@
+package com.devsoft.user_service.domain.entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UsuarioTest {
+
+    private Usuario usuario;
+
+    @BeforeEach
+    void setUp() {
+        usuario = new Usuario("13435153", "john Doe", "john.doe@example.com", "jklasjdflka", "ADMIN");
+    }
+
+    @Test
+    void testUsuarioCreacion() {
+        assertEquals("13435153", usuario.getDni());
+        assertEquals("john Doe", usuario.getNombre());
+        assertEquals("john.doe@example.com", usuario.getEmail());
+        assertEquals("jklasjdflka", usuario.getPassword());
+        assertEquals("ADMIN", usuario.getRole());
+    }
+
+}
