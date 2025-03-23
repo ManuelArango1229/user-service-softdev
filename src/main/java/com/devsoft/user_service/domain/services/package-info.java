e522447cf424f8c97b3e828cacaf3ac691ce26fc
Stiven Henao <stivenhenaoa1@gmail.com>
2025-03-23 11:46:00 -0500

    feat: implement password encryption using Spring Security

---
+++ src/main/java/com/devsoft/user_service/domain/services/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Este paquete contiene las interfaces y clases relacionadas con los servicios del dominio.
+ */
+package com.devsoft.user_service.domain.services;
---
+++ src/main/java/com/devsoft/user_service/infraestructure/database/encoder/adapter/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contiene las clases que implementan los adaptadores de la base de datos.
+ */
+package com.devsoft.user_service.infraestructure.database.encoder.adapter;
