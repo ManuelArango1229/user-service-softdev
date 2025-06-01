6f54e7c9d68c171d50c7f3b3c279474e599ba742
Victor Hernandez <victormanu968@gmail.com>
2025-05-31 23:15:48 -0500

    add: configuraciones

--- src/main/java/com/devsoft/user_service/infraestructure/rest/controller/UsuarioUpdateRestController.java
+++ src/main/java/com/devsoft/user_service/infraestructure/rest/controller/UsuarioUpdateRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,11 +17,10 @@ import com.devsoft.user_service.use_cases.UsuarioDeleteInteractor;
 import com.devsoft.user_service.use_cases.UsuarioGetByEmailInteractor;
 import com.devsoft.user_service.use_cases.UsuarioGetByIdInteractor;
 import com.devsoft.user_service.use_cases.UsuarioUpdateInteractor;
-import com.devsoft.user_service.use_cases.dtos.UsuarioUpdateDto;
 import com.devsoft.user_service.use_cases.dtos.UsuarioResponseDto;
+import com.devsoft.user_service.use_cases.dtos.UsuarioUpdateDto;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 /**
